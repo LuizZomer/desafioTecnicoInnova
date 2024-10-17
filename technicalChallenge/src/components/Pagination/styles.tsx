e01a8521d33eedcc131fec28466c311039fc9a46
Luiz Zomer <lipezomer@gmail.com>
2024-10-17 14:19:40 -0300

    fix: pagination

--- technicalChallenge/src/components/Pagination/styles.tsx
+++ technicalChallenge/src/components/Pagination/styles.tsx
@@ -4,4 +4,13 @@ export const PaginationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 500px) {
+    & :nth-child(2) {
+      font-size: 10px;
+      min-width: 100px;
+      display: flex;
+      justify-content: center;
+    }
+  }
 `;
